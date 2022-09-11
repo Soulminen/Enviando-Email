1781918d6d6c7bd1aba9ce1514324a7e65be117a
Yamil Escobar <joseyamilej@hotmail.com>
2022-09-11 22:40:31 +0200

    Enviar Email y Validaciones Terminado JS

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // Variables
 const btnEnviar = document.querySelector('#enviar');
+const btnReset = document.querySelector('#resetBtn')
 const formulario = document.querySelector('#enviar-mail');
 
 
@@ -7,6 +8,8 @@ const formulario = document.querySelector('#enviar-mail');
 const email = document.querySelector('#email');
 const asunto = document.querySelector('#asunto');
 const mensaje = document.querySelector('#mensaje');
+
+const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 eventListeners();
 function eventListeners() {
@@ -18,36 +21,65 @@ function eventListeners() {
     asunto.addEventListener('blur', validarFormulario);
     mensaje.addEventListener('blur', validarFormulario);
 
+    formulario.addEventListener('submit', enviarEmail );
+
+    btnReset.addEventListener('click', resetearFormulario);
 }
 
-
-
 // Funciones
-
 function iniciarApp() {
     btnEnviar.disabled = true;
     btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
-
 }
 
 // Valida el formulario
-
 function validarFormulario(e) {
-    
+
    if(e.target.value.length > 0) {
-    console.log('Si hay algo')
+
+    // Elimina los errores....
+        const error = document.querySelector('p.error');
+        if(error) {
+             error.remove();
+    }
+       e.target.classList.remove('border', 'border-red-500');
+       e.target.classList.add('border', 'border-green-500');
    }else {
+        e.target.classList.remove('border', 'border-green-500');
         e.target.classList.add('border', 'border-red-500');
-
-
-        mostrarError();
+        mostrarError('Todos los campos son obligatorios');
    }
+
+   if(e.target.type === 'email' ) {
+
+
+        if( er.test( e.target.value ) ) {
+            const error = document.querySelector('p.error');
+            if(error) {
+                error.remove();
+            }
+
+            e.target.classList.remove('border', 'border-red-500');
+            e.target.classList.add('border', 'border-green-500');
+        }else {
+            e.target.classList.remove('border', 'border-green-500');
+            e.target.classList.add('border', 'border-red-500');
+            mostrarError('Email no válido');
+        }
+   }
+
+   if(email.value !== ''  && asunto.value !== '' && mensaje.value !== '') {
+    btnEnviar.disabled = false;
+    btnEnviar.classList.remove('cursor-not-allowed', 'opacity-50');
+
+    }
 
 }
 
-function mostrarError() {
+
+function mostrarError(mensaje) {
     const mensajeError = document.createElement('p');
-    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.textContent = mensaje;
     mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
 
     const errores = document.querySelectorAll('.error');
@@ -55,3 +87,42 @@ function mostrarError() {
         formulario.appendChild(mensajeError);
     }
 }
+
+
+// Funcion que resetea el formulario
+
+function resetearFormulario() {
+    formulario.reset();
+    iniciarApp();
+}
+
+
+
+// Envia el email
+
+function enviarEmail(e) {
+
+    e.preventDefault();
+
+    // Mostrar el spinner
+    const spinner = document.querySelector('#spinner');
+    spinner.style.display = 'flex';
+
+    const parrafo = document.createElement('p');
+    parrafo.textContent = 'El mensaje se envió correctamente';
+    parrafo.classList.add('text-center', 'my-10', 'p-2', 'bg-green-500', 'text-white', 'font-bold', 'uppercase' );
+
+
+    //Despues de 3 segundos ocultar el spinner y mostrar el mensaje
+    setTimeout( () => {
+        spinner.style.display = 'none';
+
+        formulario.insertBefore(parrafo, spinner);
+        
+        setTimeout(() => {
+            parrafo.remove(); //Eliminar el mensaje de exito
+            resetearFormulario();
+        }, 5000);
+    }, 3000 );
+}
+
